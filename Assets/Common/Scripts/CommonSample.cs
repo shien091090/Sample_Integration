f2b456cfbde35dc4dccc10711e1c7a12ac10ca1a
NK.Shien <lithoshu@yile.com.tw>
2021-05-12 19:01:31 +0800

    1.更新gitIgnore 2.新專案 : Best Dictionary 3.調整檔案位置

---
+++ Assets/Common/Scripts/CommonSample.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonSample : MonoBehaviour
+{
+    public enum ParamType
+    {
+        INT,
+        STRING
+    }
+
+    public ParamType type;
+    public int param_int;
+    public string param_str;
+
+    public void BTN_Test()
+    {
+        switch (type)
+        {
+            case ParamType.INT:
+                Debug.Log(IsDefault(param_int));
+                break;
+
+            case ParamType.STRING:
+                Debug.Log(IsDefault(param_str));
+                break;
+        }
+
+    }
+
+    public bool IsDefault<T>(T v)
+    {
+        if (v.GetType() == typeof(string))
+        {
+            return (v.Equals(null) || v.Equals(string.Empty) || v.Equals(""));
+        }
+        else
+        {
+            return (v.Equals(default(T)));
+        }
+
+    }
+}
