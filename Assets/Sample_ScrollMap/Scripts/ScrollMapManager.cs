39eee9324da7450f23886754e3046cf3b28c1b92
NK.Shien <shien091091@gmail.com>
2021-07-19 01:35:32 +0800

    New Scene : ScrollMap

---
+++ Assets/Sample_ScrollMap/Scripts/ScrollMapManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMapManager : MonoBehaviour
+{
+    public void PrintMapPos(Vector2 pos)
+    {
+        Debug.Log(string.Format("Pos({0}, {1})", pos.x, pos.y));
+    }
+}
