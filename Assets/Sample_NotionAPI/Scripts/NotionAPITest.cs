1336eae139811afa98ce1df3a683c968f06b3e90
NK.Shien <shien091091@gmail.com>
2021-07-26 01:21:28 +0800

    NotionAPI : 1.測試用場景  2.Update Scripts

--- Assets/Sample_NotionAPI/Scripts/NotionAPITest.cs
+++ Assets/Sample_NotionAPI/Scripts/NotionAPITest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using Newtonsoft.Json;
 
 public class NotionAPITest : MonoBehaviour
@@ -50,7 +51,27 @@ public class NotionAPITest : MonoBehaviour
         }
     }
 
-    public void BTN_Test()
+    public void TEST_GET()
+    {
+        StartCoroutine(Cor_Get());
+    }
+
+    private IEnumerator Cor_Get()
+    {
+        UnityWebRequest getRequest = UnityWebRequest.Get("https://api.notion.com/v1/blocks/e5de0bc52e834047aa29742235991eb7/children");
+        getRequest.SetRequestHeader("Notion-Version", "2021-05-13");
+        getRequest.SetRequestHeader("Authorization", "secret_2JTd3rqNMZPEHIeKQioVHLNso4iXb1ESqmNW5K8mjb8");
+        getRequest.SetRequestHeader("Content-Type", "application/json");
+
+        yield return getRequest.SendWebRequest();
+
+        Debug.Log(getRequest.error);
+        Debug.Log(getRequest.responseCode);
+        Debug.Log(getRequest.downloadHandler.text);
+    }
+
+    [ContextMenu("TEST_NotionCreatePageAPI")]
+    public void TEST_NotionCreatePageAPI()
     {
         NotionPageCreator _notionNewPage = new NotionPageCreator();
 
@@ -73,6 +94,18 @@ public class NotionAPITest : MonoBehaviour
         _notionNewPage.properties.Add("rich_text", _newRichTxt);
 
         string _result = JsonConvert.SerializeObject(_notionNewPage);
+        Debug.Log(_result);
+    }
+
+    [ContextMenu("TEST_CreateNotionAPI")]
+    public void TEST_CreateNotionAPI()
+    {
+        NotionRichTextObject[] _richTxtObjArray = NotionDataGroup.GetRichTextObjectArray(
+            new RichTextSetting("TestABCTitle"),
+            new RichTextSetting("CCC", ColorType.blue),
+            new RichTextSetting("A_A", AnnotationType.bold, AnnotationType.italic));
+
+        string _result = JsonConvert.SerializeObject(_richTxtObjArray);
         Debug.Log(_result);
     }
 }
---
+++ Assets/Sample_NotionAPI/Scripts/NotionDataGroup.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotionDataGroup
+{
+    public static NotionRichTextObject[] GetRichTextObjectArray(params RichTextSetting[] settings)
+    {
+        List<NotionRichTextObject> _tempRichTxtList = new List<NotionRichTextObject>();
+
+        if (settings == null || settings.Length <= 0)
+            return _tempRichTxtList.ToArray();
+
+        for (int i = 0; i < settings.Length; i++)
+        {
+            NotionRichTextObject _richTxtObj = ParseRichTextSetting(settings[i]);
+            _tempRichTxtList.Add(_richTxtObj);
+        }
+
+        return _tempRichTxtList.ToArray();
+    }
+
+    private static NotionRichTextObject ParseRichTextSetting(RichTextSetting setting)
+    {
+        NotionRichTextObject _richTxtObj = new NotionRichTextObject();
+
+        NotionTextObject _contentTxt = new NotionTextObject();
+        _contentTxt.content = setting.content;
+
+        _richTxtObj.Text = _contentTxt;
+        _richTxtObj.annotations = ParseAnnotationSetting(setting.annotationTypes, setting.colorType);
+
+        return _richTxtObj;
+    }
+
+    private static NotionRichTextObject.AnnotationsInfo ParseAnnotationSetting(AnnotationType[] annotations, ColorType color)
+    {
+        NotionRichTextObject.AnnotationsInfo _annotationInfo = new NotionRichTextObject.AnnotationsInfo();
+
+        if(annotations != null && annotations.Length > 0)
+        {
+            foreach (AnnotationType _type in annotations)
+            {
+                switch (_type)
+                {
+                    case AnnotationType.bold:
+                        _annotationInfo.bold = true;
+                        break;
+
+                    case AnnotationType.italic:
+                        _annotationInfo.italic = true;
+                        break;
+
+                    case AnnotationType.strikethrough:
+                        _annotationInfo.strikethrough = true;
+                        break;
+
+                    case AnnotationType.underline:
+                        _annotationInfo.underline = true;
+                        break;
+
+                    case AnnotationType.code:
+                        _annotationInfo.code = true;
+                        break;
+                }
+            }
+        }
+
+        string _colorName = color.ToString().ToLower();
+        _annotationInfo.color = _colorName;
+
+        return _annotationInfo;
+    }
+}
+
+public enum AnnotationType
+{
+    bold,
+    italic,
+    strikethrough,
+    underline,
+    code
+}
+
+public enum ColorType
+{
+    Default,
+    gray,
+    brown,
+    orange,
+    yellow,
+    green,
+    blue,
+    purple,
+    pink,
+    red,
+    gray_background,
+    brown_background,
+    orange_background,
+    yellow_background,
+    green_background,
+    blue_background,
+    purple_background,
+    pink_background,
+    red_background
+}
+
+public class RichTextSetting
+{
+    public string content;
+    public ColorType colorType;
+    public AnnotationType[] annotationTypes;
+
+    public RichTextSetting(string _content, ColorType _color, params AnnotationType[] _annotations)
+    {
+        content = _content;
+        colorType = _color;
+        annotationTypes = _annotations;
+    }
+
+    public RichTextSetting(string _content, params AnnotationType[] _annotations)
+    {
+        content = _content;
+        colorType = ColorType.Default;
+        annotationTypes = _annotations;
+    }
+}
+
+public class NotionTextObject
+{
+    public string content;
+}
+
+public class NotionRichTextObject
+{
+    public class AnnotationsInfo
+    {
+        public bool bold;
+        public bool italic;
+        public bool strikethrough;
+        public bool underline;
+        public bool code;
+        public string color;
+    }
+
+    public AnnotationsInfo annotations;
+    public NotionTextObject Text;
+}
